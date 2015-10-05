1b0d678f47c5ad20ba392d7218fc023a64718457
Frode Petterson <frode.andre.petterson@gmail.com>
2015-10-05 11:04:57 +0200

    Filter data set to make sure it's valid.

--- graph-cake.js
+++ graph-cake.js
@@ -20,37 +20,73 @@ H5P.GraphCake = (function ($, EventDispatcher) {
     // Inheritance
     EventDispatcher.call(self);
 
-    // Set default behavior.
-    self.params = $.extend(true, {
-      graphMode: 'pieChart',
-      listOfTypes: [
+    // Set params and filter data set to make sure it's valid
+    self.params = params;
+    if (self.params.listOfTypes) {
+      GraphCake.filterData(self.params.listOfTypes);
+    }
+    else {
+      self.params.listOfTypes = [];
+    }
+
+    // Add example/default behavior
+    if (!self.params.listOfTypes.length) {
+      self.params.listOfTypes = [
         {
           text: 'Cat',
-          value: '1',
+          value: 1,
           color: '#D3D3D3',
           fontColor: '#000'
         },
         {
           text: 'Dog',
-          value: '2',
+          value: 2,
           color: '#ADD8E6',
           fontColor: '#000'
         },
         {
           text: 'Mouse',
-          value: '3',
+          value: 3,
           color: '#90EE90',
           fontColor: '#000'
         }
-      ]
-    }, params);
+      ];
+    }
 
+    // Keep track of type.
     self.type = (self.params.graphMode === 'pieChart' ? 'Pie' : 'Bar');
   }
 
   // Inheritance
   GraphCake.prototype = Object.create(EventDispatcher.prototype);
   GraphCake.prototype.constructor = GraphCake;
+
+  /**
+   * Make sure the data set has set the required text and value properties.
+   *
+   * @param {Array} dataSet
+   */
+  GraphCake.filterData = function (dataSet) {
+    // Cycle through data set
+    for (var i = 0; i < dataSet.length; i++) {
+      var row = dataSet[i];
+      if (row.text === undefined || row.value === undefined) {
+        // Remove invalid data
+        dataSet.splice(i, 1);
+        i--;
+        continue;
+      }
+
+      row.text = row.text.trim();
+      row.value = parseFloat(row.value);
+      if (row.text === '' || isNaN(row.value)) {
+        // Remove invalid data
+        dataSet.splice(i, 1);
+        i--;
+        continue;
+      }
+    }
+  };
 
   /**
    * Append field to wrapper.
